315008191a3d370b9b62906d85c92c87f0adf36a
Drama <abandoneddrama@foxmail.com>
2017-05-04 15:02:47 +0800

    新版官网

---
+++ src/assets/router/router.js
@@ -0,0 +1,51 @@
+// 0. 如果使用模块化机制编程，導入Vue和VueRouter，要调用 Vue.use(VueRouter)
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/login', // 登录
+    component: resolve => require(['../../component/login.vue'],
+      resolve),
+    meta: { auth: false }
+  },
+  {
+    path: '/official_website', // 官网后台
+    component: resolve => require(['../../component/official_website.vue'],
+          resolve),
+    children: [
+      {
+        path: '/addNews', // 添加新闻
+        component: resolve => require(['../../component/addNews.vue'],
+          resolve)
+      },
+      {
+        path: '/newsList', // 新闻列表
+        component: resolve => require(['../../component/newsList.vue'],
+          resolve)
+      },
+      {
+        path: '/addEmploy', // 添加招聘
+        component: resolve => require(['../../component/addEmploy.vue'],
+          resolve)
+      },
+      {
+        path: '/employList', // 招聘列表
+        component: resolve => require(['../../component/employList.vue'],
+          resolve)
+      }
+    ]
+  },
+  {
+    path: '*',
+    redirect: '/login'
+  }
+]
+
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+})
+
+// 输出router
+export default router
--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+import './assets/ElementUi'
+import './assets/public/sx_style.less'
+import router from './assets/router/router.js'
+import store from './assets/store/'
+import axios from './assets/axios/http.js'
+import Qs from 'qs'
+
+// 将axios挂载到prototype上，在组件中可以直接使用this.axios访问
+Vue.prototype.$axios = axios
+
+// 将Qs挂载到prototype上，在组件中可以直接使用this.Qs访问
+Vue.prototype.$Qs = Qs
+
+router.beforeEach(({ meta, path }, from, next) => {
+  var { auth = true } = meta
+  var isLogin = Boolean(store.state.user.userData) // true用户已登录， false用户未登录
+  if (auth && !isLogin && path !== '/login') {
+    return next({ path: '/login' })
+  }
+  next()
+})
+
 new Vue({
   el: '#app',
+  store,
+  router,
+  axios,
   render: h => h(App)
 })
